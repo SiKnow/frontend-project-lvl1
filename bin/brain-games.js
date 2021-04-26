7773dd3109e6622f6cd2bd8567940a657ec087c9
Sino Umarov <gf_nb@inbox.ru>
2021-04-26 18:53:01 +0300

    fix brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,4 @@
 #!/usr/bin/env node
-import getName from '../src/cli.js';
+import welcome from '../src/cli.js';
 
-console.log('Welcome to the Brain Games!');
-console.log('May I have your name? ');
-const name = getName();
-console.log(`Hello, ${name}!`);
+welcome();
