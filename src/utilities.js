b1227260e6f848ee5a98b9609bd56fe44f6d3fae
Sino Umarov <gf_nb@inbox.ru>
2021-05-03 17:04:36 +0300

    refix cli.js and update utilities.js

--- src/utilities.js
+++ src/utilities.js
@@ -1,3 +1,5 @@
-export const getRandom = (max) => Math.ceil(Math.random() * max);
-
-export const gamesCount = 3;
+export default (num1, num2) => {
+  const min = Math.ceil(num1);
+  const max = Math.floor(num2);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
